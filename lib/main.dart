79f895dae7d88804b608c76d4a7dad4f7dfd0e9b
Muhammad Saad Ehsan <saadehsan28@gmail.com>
2023-11-10 23:45:24 +0500

    Intro&Login

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+import 'package:todolistapp/community/communityManageTask.dart';
+import 'package:todolistapp/community/communitySplash.dart';
+import 'package:todolistapp/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: communitySplash(),
+      // home: manageTask(),
+      home: LoginPage(),
+    );
+  }
+}
